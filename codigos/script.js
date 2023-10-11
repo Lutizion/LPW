d3bc779e6f9625f3d56a074ed02d95215675797b
Luan dos Santos <141843595+Lutizion@users.noreply.github.com>
2023-10-11 10:21:24 -0300

    melhorando

--- codigos/script.js
+++ codigos/script.js
@@ -6,6 +6,49 @@ function CalcularMedia(){
     let num4 = Number(document.getElementById("numero4").value);
     let num5 = Number(document.getElementById("numero5").value);
 
-    let media = (num1+num2+num3)/3;
+    let media = (num1+num2+num3+num4+num5)/5;
     document.getElementById("resultado").innerHTML = "A média dos números é " + media;
+}
+
+function calculadora(){
+    let numero1; // primeiro número a ser digitado
+    let numero2; // segundo número a ser digitado
+        
+    let resultado; // resultado da soma dos números digitado
+    let check = document.getElementsByName("botao");
+
+    numero1 = Number(document.getElementById("num1").value);
+    numero2 = Number(document.getElementById("num2").value);
+
+   
+
+    for (let i = 0; i < check.length; i++){
+        if (check[i].checked) {
+
+    
+            if (document.getElementById("somar").checked){
+                resultado = "O Resultado da soma foi: " + (numero1+numero2) + " !!!"
+    
+            }
+            if (document.getElementById("subtrair").checked){
+                resultado = "O Resultado da subtração foi: " + (numero1-numero2) + " !!!"
+    
+            }
+            if (document.getElementById("multiplicar").checked){
+                resultado = "O Resultado da  multiplicação foi: " + (numero1*numero2) + " !!!"
+    
+            }
+            if (document.getElementById("dividir").checked){
+        
+                if(numero2 == 0){
+                    resultado = "Não existe divisão por zero";
+                }
+                else{
+                    resultado = "O Resultado da divisão foi: " + (numero1/numero2) + " !!!"
+                }
+            }
+        }
+    }
+    document.getElementById("resultado").innerHTML = resultado;
+
 }
